721d901a777ec9d54493201b1686235f57644b8a
Alexander Díaz <ingealexanderdiaz@hotmail.com>
2026-06-22 21:04:23 -0500

    Configuracion perfl del socio

--- backend/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controllers/authController');
-const { requireAuth } = require('../middleware/auth');
-
-router.post('/login', authController.login);
-router.get('/perfil', requireAuth, authController.getProfile);
-router.put('/cambiar-password', requireAuth, authController.changePassword);
-
-module.exports = router;
